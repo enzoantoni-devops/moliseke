79c89e209cb1b2f3a5d5950d1b87810258d070f5
Enzo <75916622+enzoantoni-devops@users.noreply.github.com>
2025-09-11 18:57:50 -0300

    feat: show region flags and platform logos

--- src/components/Regions.jsx
+++ src/components/Regions.jsx
@@ -1,16 +1,43 @@
 import React from 'react';
 import styles from '../App.module.css';
 
+const regions = [
+  { name: 'United States', code: 'us' },
+  { name: 'Canada', code: 'ca' },
+  { name: 'United Kingdom', code: 'uk' },
+  { name: 'European Union', code: 'eu' },
+  { name: 'Australia', code: 'au' },
+];
+
+const logos = [
+  { name: 'Microsoft 365', file: '365' },
+  { name: 'AWS', file: 'aws' },
+  { name: 'Azure', file: 'azure' },
+  { name: 'Elasticsearch', file: 'elasticsearch' },
+  { name: 'GitHub', file: 'github' },
+  { name: 'GitLab', file: 'gitlab' },
+  { name: 'SharePoint', file: 'sharepoint' },
+  { name: 'Slack', file: 'slack' },
+  { name: 'Teams', file: 'teams' },
+  { name: 'Windows', file: 'windows' },
+];
+
 export default function Regions() {
   return (
     <section id='regions' className={styles.sectionPad}>
       <div className='container'>
         <h2 className={styles.sectionHeading}>Regions we serve</h2>
         <div className={styles.regionsGrid}>
-          {['United States', 'Canada', 'United Kingdom', 'European Union', 'Australia'].map(r => (
-            <span key={r} className='chip'>
-              {r}
+          {regions.map(({ name, code }) => (
+            <span key={name} className={`chip ${styles.flagChip}`}>
+              <img src={`/flags/${code}.svg`} alt='' width='20' height='20' />
+              {name}
             </span>
+          ))}
+        </div>
+        <div className={styles.logosGrid}>
+          {logos.map(({ name, file }) => (
+            <img key={file} src={`/logos/${file}.svg`} alt={`${name} logo`} loading='lazy' />
           ))}
         </div>
       </div>
